11e8d3688a47b33c9b7d08919d501e1daa188906
JOSÉ BRUNO OLIVEIRA DA SILVA <jose-bruno_silva@estudante.sesisenai.org.br>
2025-04-28 20:00:21 -0300

    prova 28/04

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,64 +5,81 @@ import { useEffect, useState } from 'react';
 
 type Item = {
   id: UUID
-  text: "",
+  text: string,
   checked: false
 }
 
 export default function MarketList() {
   const [item, setItem] = useState<Item[]>([]);
+  const [text, setText] = useState<string>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   useEffect(() => {
     loadItens();
   }, [])
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
+  
   async function loadItens() {
-    const response = await axios.get("http://localhost:3001/db.json");
-    
-  }
-
-  async function handleAddItem() {
-   
-    // CRIAR O OBJETO DO ITEM
-    const item = {
-      id:randomUUID,
-      nome:""
-    }
-    
-    // CHAMA A API PARA ADICIONAR O ITEM
-    await axios.post("http://localhost:3000/posts", item);
-
+    const response = await axios.get("http://localhost:3001/itens");
+    setItem(response.data);
     loadItens();
   }
 
-  function handleRemoveItem(id: string) {
+  async function handleAddItem() {
+    // CRIAR O OBJETO DO ITEM
+    const itens = {
+      id: randomUUID,
+      text: text,
+      checked: false
+    }
+    // CHAMA A API PARA ADICIONAR O ITEM
+    await axios.post("http://localhost:3002/itens", itens)
+    setText('');
+    loadItens();
+  }
+
+  async function handleRemoveItem(id: string) {
+    await axios.delete(`http://localhost:3001/itens/${id}`)
     // FILTRA O ESTADO E REMOVE O ITEM
     // CHAMA A API PARA REMOVER O ITEM
     // CARREGA OS PRODUTOS NOVAMENTE // loadItens();
+    loadItens();
   }
-
-  function handleUpdateItem(id: string) {
-    // CRIA O OBJETO DO ITEM
+  
+  async function handleUpdateItem(id: string, checked: boolean ) {
+    
     // CHAMA A API PARA ATUALIZAR O ITEM
+    await axios.patch(`http://localhost:3001/itens/${id}`,{checked:checked})
+    
+    // cheack box
+    // CRIA O OBJETO DO ITEM
     // CARREGA OS PRODUTOS NOVAMENTE // loadItens();
+    loadItens();
   }
 
   return (
     <div className="container">
+    <div>
+      <h1>MarketList</h1>
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        placeholder="Adicione um novo item"
+      />
+      <button onClick={handleAddItem}>Adicionar item</button>
+    </div>
+    <div className="content">
       <div>
-        <h1>MarketList</h1>
-        <input type="text" placeholder='Adicione um novo item' />
-        <button onClick={handleAddItem}>Adicionar item</button>
-      </div>
-      <div className='content'>
-      {isLoading ? (
-                        <h1></h1>
-                    ) : (
-                        item.map(item => (
-                            <Item post={item} key={item.id} setItem={setItem} />
-                        ))
-                    )}
+        
+        {item.map(item => (
+          <input type='cheackbox' onClick={handleUpdateItem} key={item.id}>
+            <span>{text}</span>
+            <span>{item.text}</span>
+            {item.text} <button onClick={() => handleRemoveItem(item.id)}>Remover</button>
+          </input>
+        ))}
       </div>
     </div>
-  );
+  </div>
+);
 }
